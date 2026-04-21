65657f09fe1702f6f2f6f9d93ac8671c98c38922
Matthew Walster <matthew@walster.org>
2026-04-21 19:53:49 +0000

    test: add performance benchmarks for metadata message allocation

---
+++ server/msgcache_bench_test.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"testing"
+	"github.com/dotwaffle/ninep/proto"
+)
+
+func BenchmarkMetadataAlloc(b *testing.B) {
+	// Setup a mock connection with protocolL.
+	c := &conn{protocol: protocolL}
+
+	types := []proto.MessageType{
+		proto.TypeTremove,
+		proto.TypeTmkdir,
+		proto.TypeTsetattr,
+		proto.TypeTrename,
+		proto.TypeTsymlink,
+		proto.TypeTmknod,
+	}
+
+	for _, t := range types {
+		b.Run("type="+t.String(), func(b *testing.B) {
+			b.ReportAllocs()
+			for b.Loop() {
+				msg, err := c.newMessage(t)
+				if err != nil {
+					b.Fatal(err)
+				}
+				// Return to cache.
+				putCachedMsg(msg)
+			}
+		})
+	}
+}
